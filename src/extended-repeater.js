412e76f84cd1c12b824428a9df84742955345c5d
Yuliya Zabalotnaya <yuv.zabolotnaya@gmail.com>
2022-10-17 02:29:23 +0300

    resolve tasks exept one

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,35 @@ const { NotImplementedError } = require("../extensions/index.js");
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(str, options) {}
+function repeater(str, options) {
+  let result = [];
+  let addition = [];
+
+  options.separator ? options.separator : (options.separator = "+");
+  options.additionSeparator
+    ? options.additionSeparator
+    : (options.additionSeparator = "|");
+
+  options.addition === false || options.addition === null || options.addition
+    ? options.addition
+    : (options.addition = "");
+
+  options.repeatTimes ? options.repeatTimes : (options.repeatTimes = 1);
+  options.additionRepeatTimes
+    ? options.additionRepeatTimes
+    : (options.additionRepeatTimes = 1);
+
+  for (let i = 0; i < options.additionRepeatTimes; i++) {
+    addition.push(String(options.addition));
+  }
+
+  for (let i = 0; i < options.repeatTimes; i++) {
+    result.push(String(str) + addition.join(options.additionSeparator));
+  }
+  // console.log(addition);
+
+  return result.join(options.separator);
+}
 
 module.exports = {
   repeater,
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -25,62 +25,72 @@ class VigenereCipheringMachine {
   // }
 
   encrypt(string, key) {
-    if (arguments.length !== 2 && arguments.length === 0) {
+    if (
+      string === undefined ||
+      string === null ||
+      string === "" ||
+      key === undefined ||
+      key === null ||
+      key === ""
+    )
       throw new Error("Incorrect arguments!");
-    } else {
-      let result = "";
 
-      for (let i = 0, j = 0; i < string.length; i++) {
-        const c = string.charAt(i);
-        // if (isLetter(c)) {
-        if (c === c.toUpperCase()) {
-          result += String.fromCharCode(
-            ((c.charCodeAt(0) + key.toUpperCase().charCodeAt(j) - 2 * 65) %
-              26) +
-              65
-          );
-        } else {
-          result += String.fromCharCode(
-            ((c.charCodeAt(0) + key.toLowerCase().charCodeAt(j) - 2 * 97) %
-              26) +
-              97
-          );
-        }
-        // } else {
-        //   result += c;
-        // }
-        j = ++j % key.length;
+    let result = "";
+
+    for (let i = 0, j = 0; i < string.length; i++) {
+      const c = string.charAt(i);
+      // if (isLetter(c)) {
+      if (c === c.toUpperCase()) {
+        result += String.fromCharCode(
+          ((c.charCodeAt(0) + key.toUpperCase().charCodeAt(j) - 2 * 65) % 26) +
+            65
+        );
+      } else {
+        result += String.fromCharCode(
+          ((c.charCodeAt(0) + key.toLowerCase().charCodeAt(j) - 2 * 97) % 26) +
+            97
+        );
       }
-      return result.toUpperCase();
+      // } else {
+      //   result += c;
+      // }
+      j = ++j % key.length;
     }
+    return result.toUpperCase();
   }
-  decrypt(encryptedMessage, key) {
-    if (arguments.length !== 2 && arguments.length === 0) {
-      throw new Error("Incorrect arguments!");
-    } else {
-      let result = "";
 
-      for (let i = 0, j = 0; i < encryptedMessage.length; i++) {
-        const c = encryptedMessage.charAt(i);
-        // if (isLetter(c)) {
-        if (c === c.toUpperCase()) {
-          result += String.fromCharCode(
-            90 -
-              ((25 - (c.charCodeAt(0) - key.toUpperCase().charCodeAt(j))) % 26)
-          );
-        } else {
-          result += String.fromCharCode(
-            122 -
-              ((25 - (c.charCodeAt(0) - key.toLowerCase().charCodeAt(j))) % 26)
-          );
-        }
-        // } else {
-        //   result += c;
-        // }
-        j = ++j % key.length;
+  decrypt(encryptedMessage, key) {
+    if (
+      encryptedMessage === undefined ||
+      encryptedMessage === null ||
+      encryptedMessage === "" ||
+      key === undefined ||
+      key === null ||
+      key === ""
+    )
+      throw new Error("Incorrect arguments!");
+
+    let result = "";
+
+    for (let i = 0, j = 0; i < encryptedMessage.length; i++) {
+      const c = encryptedMessage.charAt(i);
+      // if (isLetter(c)) {
+      if (c === c.toUpperCase()) {
+        result += String.fromCharCode(
+          90 - ((25 - (c.charCodeAt(0) - key.toUpperCase().charCodeAt(j))) % 26)
+        );
+      } else {
+        result += String.fromCharCode(
+          122 -
+            ((25 - (c.charCodeAt(0) - key.toLowerCase().charCodeAt(j))) % 26)
+        );
       }
-      return result.toUpperCase();
+      // } else {
+      //   result += c;
+      // }
+      j = ++j % key.length;
     }
+    return result.toUpperCase();
   }
 }
 
